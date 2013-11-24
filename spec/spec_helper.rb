7e616b01c0eafe17c834f49aa85d593a3d1ed5f9
Perry Smith <pedz@easesoftware.com>
2013-11-24 15:53:23 -0600

    Still mucking about

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 
-$LOAD_PATH.unshift File.expand_path("../..", __FILE__)
+# Not needed if odm is in a lib.  Still undecided how I want to do
+# this.
+# $LOAD_PATH.unshift File.expand_path("../..", __FILE__)
