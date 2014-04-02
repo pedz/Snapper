81d56d81046f5d5f5a7f5299eab04905971d9bdd
Perry Smith <pedz@easesoftware.com>
2014-04-02 18:53:52 -0500

    Added vasi to netstat -v parsing.

--- lib/dot_file/netstat_v.rb
+++ lib/dot_file/netstat_v.rb
@@ -1,7 +1,7 @@
 
 # Class for the netstat -v output
 class Netstat_v < DotFileParser::Base
-  T = Regexp.new("(FIBRE CHANNEL STATISTICS REPORT: (.*)|ETHERNET STATISTICS \\((.*)\\) :)\n")
+  T = Regexp.new("(FIBRE CHANNEL STATISTICS REPORT: (.*)|ETHERNET STATISTICS \\((.*)\\) :|VASI STATISTICS \\((.*)\\) :)\n")
 
   def initialize(text)
     @text = text
@@ -10,7 +10,8 @@ class Netstat_v < DotFileParser::Base
     while true
       whole_line, device_name, rest = parts.shift(3)
       break if whole_line.nil?
-      # puts device_name
+      puts device_name
+      puts rest.match('.*')
     end
   end
 end
