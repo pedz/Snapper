e748c44d167207721c39f733b9aca8d69a47f7ab
Perry Smith <pedz@easesoftware.com>
2015-11-04 11:40:28 -0600

    Added hash_mixins to db
    It now creates methods.  It turns out that this is doubling the CPU
    time (the change to Item in particular) but I'm going to keep it for
    now.  It is taking about 6 seconds per snap so I think even with 5
    snaps, it should be within a comfortable user time.

--- lib/db.rb
+++ lib/db.rb
@@ -1,10 +1,13 @@
 require_relative 'logging'
+require_relative 'hash_mixins'
 require 'json'
 
 # A place to store things
 class Db < Hash
   include Logging
   LOG_LEVEL = Logger::INFO      # Logging level for the Db class
+
+  include HashMakeMethods
 
   # Adds item into the database by pushing item onto the table from
   # the database who name matches the class of the item.
