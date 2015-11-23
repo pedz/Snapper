1261316bad2941459239306000737bac1c75225f
Perry Smith <pedz@easesoftware.com>
2015-11-23 15:32:32 -0600

    Yet more documentation.  Up to logging.

--- lib/list.rb
+++ lib/list.rb
@@ -1,5 +1,8 @@
 require_relative 'logging'
 
-# This is just an array that understands our odd printing system.
+# This is not really used yet.  To be useful, it needs to use the same
+# missing_method tech that Item uses and then the print in Enumerable
+# could be moved into here.  But, so far, the current set up hasn't
+# bit me yet so I'm going to leave it alone for now.
 class List < Array
 end
--- lib/logging.rb
+++ lib/logging.rb
@@ -2,10 +2,17 @@ require 'logger'
 
 # This logging module creates a logger per class that include it.
 # Both class and instance methods within the same class will have
-# access to the same Logger via the logger method
+# access to the same Logger via the logger method.  This leaves the
+# door open for a lot of features such as different classes logging to
+# different files as well as the ability to set per class logging
+# level which could be done from the command line.
 module Logging
   def self.included(base)
     class << base
+      ##
+      # :singleton-method:
+      # The .logger method is available from class methods and is
+      # actually where all the work takes place.
       def logger
         unless @logger
           @logger = Logger.new($stderr)
@@ -19,12 +26,17 @@ module Logging
         @logger
       end
       
+      ##
+      # :singleton-method:
+      # The logger may be assigned to a new logging instance.
       def logger=(logger)
         @logger = logger
       end
     end
   end
   
+  # The #logger method is also available from any instance method and
+  # calls to Logging.logger.
   def logger
     self.class.logger
   end
