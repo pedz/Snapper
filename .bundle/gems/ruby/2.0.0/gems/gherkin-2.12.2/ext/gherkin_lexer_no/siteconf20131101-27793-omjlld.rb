77b42688f3fefe2adc2c95147a1762a224469ccf
Perry Smith <pedz@easesoftware.com>
2015-12-24 12:06:34 -0600

    Removed old ruby 2.0.0 gems

--- .bundle/gems/ruby/2.0.0/gems/gherkin-2.12.2/ext/gherkin_lexer_no/siteconf20131101-27793-omjlld.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131101-27793-1om1l1x"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
--- .bundle/gems/ruby/2.0.0/gems/gherkin-2.12.2/ext/gherkin_lexer_ru/siteconf20131101-27793-hvs6lv.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131101-27793-1ka1rge"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
