727db5113a2cf4b82c8a10b1d001efe8572e877b
Perry Smith <pedz@easesoftware.com>
2015-12-25 14:53:51 -0600

    Fixed yard-rspec to handle context

--- .bundle/gems/ruby/2.2.0/gems/yard-rspec-0.1/lib/yard-rspec/handler.rb
+++ .bundle/gems/ruby/2.2.0/gems/yard-rspec-0.1/lib/yard-rspec/handler.rb
@@ -1,3 +1,6 @@
+# The assumption is that the describe will have either two arguments
+# or be nested two deep with Class and #method being the two levels.
+# This allows the P in the +it+ handler to find the method.
 class RSpecDescribeHandler < YARD::Handlers::Ruby::Base
   handles method_call(:describe)
   
@@ -7,8 +10,33 @@ class RSpecDescribeHandler < YARD::Handlers::Ruby::Base
       src = statement.parameters[1].jump(:string_content).source
       objname += (src[0] == "#" ? "" : "::") + src
     end
-    obj = {:spec => owner ? (owner[:spec] || "") : ""}
+    obj = {
+      spec: (owner ? (owner[:spec] || "") : ""),
+      context: ""
+    }
     obj[:spec] += objname
+
+    parse_block(statement.last.last, owner: obj)
+  rescue YARD::Handlers::NamespaceMissingError
+  end
+end
+
+# Within the two +describe+ nestings and around all of the +it+ blocks
+# can be an optional set of +context+ nestings.
+class RSpecContextHandler < YARD::Handlers::Ruby::Base
+  handles method_call(:context)
+  
+  def process
+    return unless statement.is_a?(YARD::Parser::Ruby::MethodCallNode)
+    context = statement.parameters.first.jump(:string_content).source
+    # If owner does not have a :spec, then this context is not within
+    # the describe.
+    # @todo add nested context support
+    obj = {
+      spec: (owner ? (owner[:spec] || "") : ""),
+      context: context
+    }
+
     parse_block(statement.last.last, owner: obj)
   rescue YARD::Handlers::NamespaceMissingError
   end
@@ -22,11 +50,22 @@ class RSpecItHandler < YARD::Handlers::Ruby::Base
     obj = P(owner[:spec])
     return if obj.is_a?(Proxy)
     
-    (obj[:specifications] ||= []) << {
-      name: statement.parameters.first.jump(:string_content).source,
-      file: statement.file,
-      line: statement.line,
-      source: statement.last.last.source.chomp
+    # @todo can not cope with "one-liner" syntax
+    #   it { is_expected.to respond_to(:foo_dog) }
+    return if statement.parameters.first.nil?
+
+    name = statement.parameters.first.jump(:string_content).source
+    file = statement.file
+    line = statement.line
+    source = statement.last.last.source.chomp
+    h = {
+      name: name,
+      file: file,
+      line: line,
+      source: source
     }
+
+    obj[:specifications] ||= {}
+    (obj[:specifications][owner[:context]] ||= []) << h
   end
 end
