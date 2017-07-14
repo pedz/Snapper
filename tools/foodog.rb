55542777969f1a0988566abf2bf6ee4ea2df41dd
Perry Smith <pedz@easesoftware.com>
2017-07-14 12:29:50 -0500

    Checkin so I can move to new name

--- tools/foodog.rb
+++ tools/foodog.rb
@@ -1,32 +1,37 @@
 #!/usr/bin/env ruby
 
+require 'pathname'
 require 'net/ssh'
 require 'set'
 
 PSQL="/gsa/ausgsa/projects/r/ruby/pgsql/bin/psql"
 $defects = Set.new
 
-blah = File.open("new_code.rb", "w", 0644)
-blah.puts "# This file is automatically produced by #{__FILE__}"
-blah.puts "# array of issues.  each issue is an array:"
-blah.puts "# [ injecting defect, resolving defect, text ]"
-blah.puts "$issues = ["
+base = Pathname(__FILE__).dirname.dirname
+issues_rb = base + "issues.rb"
+issues_txt = base + "tools/issues.txt"
+
+new_code = issues_rb.open("w", 0644)
+new_code.puts "# This file is automatically produced by #{__FILE__}"
+new_code.puts "# array of issues.  each issue is an array:"
+new_code.puts "# [ injecting defect, resolving defect, text ]"
+new_code.puts "$issues = ["
 previous_line = nil
-File.open(__FILE__ + "/../issues.txt") do |file|
+issues_txt.open do |file|
   file.each_line do |line|
     next if /^#/.match(line)
     fields = line.split('|')
     $defects.add(fields[0].strip)
     $defects.add(fields[1].strip)
-    blah.puts "#{previous_line}," unless previous_line.nil?
+    new_code.puts "#{previous_line}," unless previous_line.nil?
     previous_line = "  [ #{fields[0].strip.inspect}, #{fields[1].strip.inspect}, #{fields[2].strip.inspect} ]"
   end
 end
-blah.puts previous_line unless previous_line.nil?
-blah.puts "]"
-blah.puts
-blah.puts "# Hash that maps a defect to a list of APARs"
-blah.puts "$defect2apars = {"
+new_code.puts previous_line unless previous_line.nil?
+new_code.puts "]"
+new_code.puts
+new_code.puts "# Hash that maps a defect to a list of APARs"
+new_code.puts "$defect2apars = {"
 previous_line = nil
 Net::SSH.start('condor.austin.ibm.com', 'condor') do |ssh|
   $defects.each do |defect|
@@ -34,10 +39,10 @@ Net::SSH.start('condor.austin.ibm.com', 'condor') do |ssh|
     raise Exception.new("psql failed") unless output.exitstatus == 0
     output = output.split("\n")
     output.delete("NEEDS_APAR")
-    blah.puts "#{previous_line}," unless previous_line.nil?
+    new_code.puts "#{previous_line}," unless previous_line.nil?
     previous_line = "  #{defect.inspect} => [ \"#{output.join('", "')}\" ]"
   end
 end
-blah.puts previous_line unless previous_line.nil?
-blah.puts "}"
-blah.close
+new_code.puts previous_line unless previous_line.nil?
+new_code.puts "}"
+new_code.close
